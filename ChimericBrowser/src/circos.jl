926be6b42e39e8575a5ae8029d499dff7643c7be
Malte Siemers <malte.siemers@mailbox.org>
2022-09-13 18:58:04 +0200

    restructure files

--- ChimericBrowser/src/circos.jl
+++
@@ -1,5 +0,0 @@
-function circos(; kwargs...)
-    available_props = Symbol[:id, :config, :enableDownloadSVG, :enableZoomPan, :eventDatum, :layout, :loading_state, :selectEvent, :size, :style, :tracks]
-    wild_props = Symbol[]
-    return Component("circos", "Circos", "dash_bio", available_props, wild_props; kwargs...)
-end
--- ChimericBrowser/src/deps.jl
+++ ChimericBrowser/src/deps.jl
@@ -1,3 +1,9 @@
+function circos(; kwargs...)
+    available_props = Symbol[:id, :config, :enableDownloadSVG, :enableZoomPan, :eventDatum, :layout, :loading_state, :selectEvent, :size, :style, :tracks]
+    wild_props = Symbol[]
+    return Component("circos", "Circos", "dash_bio", available_props, wild_props; kwargs...)
+end
+
 function cytoscape(; kwargs...)
     available_props = Symbol[:id, :autoRefreshLayout, :autolock, :autoungrabify, :autounselectify, :boxSelectionEnabled, :className,
                                 :elements, :generateImage, :imageData, :layout, :maxZoom, :minZoom, :mouseoverEdgeData, :mouseoverNodeData,
