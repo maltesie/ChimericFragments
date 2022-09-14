9aa9f3f4c0952706de91d62e48f0d7a923c54cf9
Malte Siemers <malte.siemers@mailbox.org>
2022-09-14 15:59:40 +0200

    fix

--- ChimericBrowser/src/ChimericBrowser.jl
+++ ChimericBrowser/src/ChimericBrowser.jl
@@ -17,7 +17,7 @@ function chimeric_browser(results_folder::String, genome_file::String)
     interactions_dfs, singles_dfs, stats_dfs, gene_names, genome_info = load_data(results_folder, genome_file)
 
     app = dash(assets_folder=assets_path)
-    app.layout = browser_layout(sort([k for k in keys(interactions_dfs)]), ["test"], genome_info)
+    app.layout = browser_layout(sort([k for k in keys(interactions_dfs)]), [Dict("label"=>"test", "value"=>"test")], genome_info)
 
     update_selection_callback!(app, interactions_dfs)
 
