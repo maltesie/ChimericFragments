5beefa779137646252d6e35cf933c9643ebc7e0c
Malte Siemers <malte.siemers@mailbox.org>
2022-09-14 14:38:07 +0200

    intermediate

--- ChimericBrowser/src/callbacks.jl
+++ ChimericBrowser/src/callbacks.jl
@@ -1,4 +1,4 @@
 test_callback!(app::Dash.DashApp) = 
-callback!(app, Input("reads-slider", "value"), Output("info-output", "children")) do slider_value
+callback!(app, Output("info-output", "children"), Input("reads-slider", "value")) do slider_value
     "$slider_value"
 end
