9d389884bbdc01b678483fcb790317031cef8de2
Malte Siemers <malte.siemers@mailbox.org>
2022-10-07 14:56:31 +0200

    allow full paths in config

--- analyze.jl
+++ analyze.jl
@@ -11,37 +11,39 @@ include(config_file)
 
 project_path = dirname(config_file)
 
-isfile(joinpath(project_path, annotation_file)) || throw(AssertionError("Cannot find a file with the filename $annotation_file. Please edit config.jl."))
-isfile(joinpath(project_path, genome_file)) || throw(AssertionError("Cannot find a file with the filename $genome_file. Please edit config.jl."))
+isfile(annotation_file) || (annotation_file = joinpath(project_path, annotation_file))
+isfile(annotation_file) || throw(AssertionError("Cannot find $annotation_file. Please edit config.jl."))
+isfile(genome_file) || (genome_file = joinpath(project_path, genome_file))
+isfile(genome_file) || throw(AssertionError("Cannot find $genome_file. Please edit config.jl."))
 
-data_path = joinpath(project_path, data_folder)
-isdir(joinpath(project_path, data_folder)) || throw(AssertionError("Cannot find a directory called $data_folder. Please edit config.jl."))
+isdir(data_folder) || (data_folder = joinpath(project_path, data_folder))
+isdir(data_folder) || throw(AssertionError("Cannot find a directory called $data_folder. Please edit config.jl."))
 
 Pkg.activate(joinpath(@__DIR__, "ChimericAnalysis"))
 Pkg.instantiate()
 
 using ChimericAnalysis, BioSequences
 
-fastqs = is_paired_end ? PairedSingleTypeFiles([(joinpath(data_path, sname*suffix_read1*".fastq.gz"), joinpath(data_path, sname*suffix_read2*".fastq.gz"))
+fastqs = is_paired_end ? PairedSingleTypeFiles([(joinpath(data_folder, sname*suffix_read1*".fastq.gz"), joinpath(data_folder, sname*suffix_read2*".fastq.gz"))
                                                 for (sname, _) in samplename_condition]) :
-                            SingleTypeFiles([joinpath(data_path, sname*".fastq.gz") for (sname, _) in samplename_condition])
+                            SingleTypeFiles([joinpath(data_folder, sname*".fastq.gz") for (sname, _) in samplename_condition])
 check_files_exist(fastqs)
 
-genome = Genome(joinpath(project_path, genome_file))
+genome = Genome(genome_file)
 
 types = vcat([srna_type, cds_type, rrna_type, trna_type], additional_types)
-features = Features(joinpath(project_path, annotation_file), types)
+features = Features(annotation_file, types)
 if autocomplete_utrs
     add5utrs!(features; cds_type=cds_type, utr_type=fiveutr_type, utr_length=autocomplete_utr_length)
     add3utrs!(features; cds_type=cds_type, utr_type=threeutr_type, utr_length=autocomplete_utr_length)
 else
-    merge!(features, Features(joinpath(project_path, annotation_file), [threeutr_type, fiveutr_type]))
+    merge!(features, Features(annotation_file, [threeutr_type, fiveutr_type]))
 end
 
 if autocomplete_igrs
     addigrs!(features; igr_type=igr_type)
 else
-    merge!(features, Features(joinpath(project_path, annotation_file), [igr_type]))
+    merge!(features, Features(annotation_file, [igr_type]))
 end
 
 features = mergetypes(features, [threeutr_type, fiveutr_type, cds_type], "UTRS_CDS")
