0c036fb17a55a2c8b61581c83e4a52f34190398b
Malte Siemers <malte.siemers@mailbox.org>
2023-02-13 11:39:17 +0100

    update RNASeqTools

--- install.jl
+++ install.jl
@@ -1,17 +1,10 @@
 using Pkg
 
 Pkg.activate("ChimericAnalysis")
-Pkg.add(url="https://github.com/maltesie/RNASeqTools#master")
 Pkg.instantiate()
 Pkg.build()
 Pkg.activate("ChimericBrowser")
 Pkg.instantiate()
 Pkg.build()
-#Pkg.add(url="https://github.com/maltesie/RNASeqTools#master")
-#Pkg.build("RNASeqTools")
-#Pkg.develop(path="./ChimericAnalysis")
-#Pkg.build("ChimericAnalysis")
-#Pkg.develop(path="./ChimericBrowser")
-#Pkg.build("ChimericBrowser")
 
 println("Install finished.")
