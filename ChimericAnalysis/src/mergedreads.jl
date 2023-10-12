df4ae83034dba625c01dffb68542fa9a0d055b16
Malte Siemers <malte.siemers@mailbox.org>
2023-10-12 13:44:57 +0200

    add comments to code

--- ChimericAnalysis/src/mergedreads.jl
+++ ChimericAnalysis/src/mergedreads.jl
@@ -1,28 +1,45 @@
+# Merged alignments with regard to their presence on reads 1 and 2 in paired end illumina sequencing data.
 struct MergedAlignedRead
     alns::AlignedReads
     pindexpairs::Vector{Tuple{Int,Int}}
     merged::Vector{Bool}
 end
 
+# Empty constructor
 MergedAlignedRead(alnreads::AlignedReads) = MergedAlignedRead(alnreads, Tuple{Int, Int}[], Bool[])
 
+# Number of merged alignments in the struct
 Base.length(mergedread::MergedAlignedRead) = length(mergedread.pindexpairs)
 
+# Two alignments on one read can be merged, if they are on different reads, have the same annotation and if the leftmost coordinate of
+# the alignment on read 1 is upstream or equal to the leftmost coordinate of the alignment on read 2.
 canmerge(alns::AlignedReads, i1::Int, i2::Int) = alns.annotated[i2] && (alns.reads[i1]!==alns.reads[i2]) && (alns.annames[i1]===alns.annames[i2]) &&
 ((alns.strands[i1] == STRAND_POS) ? (alns.leftpos[i1]<=alns.leftpos[i2]) : (alns.rightpos[i1]>=alns.rightpos[i2]))
 
+# Do the merge on preallocated mergedread for efficiency.
 function mergeparts!(mergedread::MergedAlignedRead, alnread::AlignedRead)
     alns = alnread.alns
+
+    # Setup a reference to the alignments to be merged
     index = @view alns.pindex[alnread.range]
+
+    # Set preallocated mergedread to appropriate size and overwrite with defaults
     length(mergedread.pindexpairs) >= length(index) || resize!(mergedread.pindexpairs, length(index))
     length(mergedread.merged) >= length(index) || resize!(mergedread.merged, length(index))
     mergedread.merged .= false
     c::Int = 0
+
+    # loop through all alignments
     for (i::Int, ii::Int) in enumerate(index)
+
+        # skip if current alignment was already merged or has no annotation
         (mergedread.merged[i] || !alns.annotated[ii]) && continue
+
         c += 1
+        # Set index of remaining alignments to check for merging
         nextolpi = i+1
 
+        # loop through remaining alignments on read until one can be merged
         if nextolpi <= length(index)
             while !canmerge(alns, ii, index[nextolpi])
                 nextolpi += 1
@@ -30,6 +47,7 @@ function mergeparts!(mergedread::MergedAlignedRead, alnread::AlignedRead)
             end
         end
 
+        # merge if merable alignment was found and mark as merged.
         if nextolpi > length(index)
             mergedread.pindexpairs[c] = (ii, ii)
         else
@@ -41,12 +59,14 @@ function mergeparts!(mergedread::MergedAlignedRead, alnread::AlignedRead)
     return mergedread
 end
 
+# Check the space between alignments on one read to see if there is a ligation point.
 function hasligationpoint(mergedread::MergedAlignedRead, pair1::Tuple{Int,Int}, pair2::Tuple{Int, Int}; max_distance=5)
     alns = mergedread.alns
     return ((alns.reads[first(pair1)]==alns.reads[first(pair2)]) && (alns.read_leftpos[first(pair2)]-alns.read_rightpos[first(pair1)] <= max_distance)) ||
     ((alns.reads[last(pair1)]==alns.reads[last(pair2)]) && (alns.read_leftpos[last(pair2)]-alns.read_rightpos[last(pair1)] <= max_distance))
 end
 
+# Compute overlap or distance between two intervals, if order is checked, interval 1 has to be upstream of interval 2 for finite distance
 function distance(l1::Int, r1::Int, s1::Strand, l2::Int, r2::Int, s2::Strand; check_order=false)::Float64
     s1 != s2 && return Inf
     check_order && (s1 == STRAND_POS ? l1 > l2 : r2 < r1) && return Inf
@@ -55,8 +75,12 @@ function distance(l1::Int, r1::Int, s1::Strand, l2::Int, r2::Int, s2::Strand; ch
     return 0
 end
 
+# A pair of alignments is chimeric.
 function ischimeric(mergedread::MergedAlignedRead, pair1::Tuple{Int,Int}, pair2::Tuple{Int, Int}; min_distance=1000, check_annotation=true, check_order=false)
+    # if enabled, check annotation names
     check_annotation && (mergedread.alns.annames[first(pair1)] == mergedread.alns.annames[first(pair2)]) && return false
+    # if enabled, include order of appearance on the reads. If something upstream of something else in the reference appears in the wrong order,
+    # the alignments are classified as chimeric. This is how self-chimeras are defined.
     return distance(
         mergedread.alns.leftpos[first(pair1)],
         mergedread.alns.rightpos[last(pair1)],
@@ -66,6 +90,7 @@ function ischimeric(mergedread::MergedAlignedRead, pair1::Tuple{Int,Int}, pair2:
         mergedread.alns.strands[first(pair2)]; check_order=check_order) > min_distance
 end
 
+# Check if any pair of alignments on a read is chimeric
 function ischimeric(mergedread::MergedAlignedRead; min_distance=1000, check_annotation=true, check_order=false)
     length(mergedread) > 1 || return false
     for (i, p1) in enumerate(mergedread.pindexpairs), p2 in (@view mergedread.pindexpairs[i+1:end])
@@ -74,6 +99,7 @@ function ischimeric(mergedread::MergedAlignedRead; min_distance=1000, check_anno
     return false
 end
 
+# Check if there are self-chimeric interactions on the read (see comments in ischimeric)
 function isselfchimeric(mergedread::MergedAlignedRead; min_distance=1000)
     length(mergedread) > 1 || return false
     for (i, p1) in enumerate(mergedread.pindexpairs), p2 in (@view mergedread.pindexpairs[i+1:end])
@@ -86,6 +112,7 @@ function isselfchimeric(mergedread::MergedAlignedRead; min_distance=1000)
     return false
 end
 
+# Check if a read has only one (merged) alignment. If for paired end sequencing only one of two reads map, do not classify as single.
 function issingle(mergedread::MergedAlignedRead; is_paired_end=true)
     if (length(mergedread) == 1)
         return is_paired_end != (mergedread.pindexpairs[1][1] == mergedread.pindexpairs[1][2])
@@ -93,10 +120,12 @@ function issingle(mergedread::MergedAlignedRead; is_paired_end=true)
     return false
 end
 
+# Compute hash from name and type of annotation for efficient indexing
 function myhash(alns::AlignedReads, i::Int; use_type=true)
     return use_type ? hash(alns.annames[i], hash(alns.antypes[i])) : hash(alns.annames[i])
 end
 
+# Compute length of a merged alignment by combining info from both parts on reads 1 and 2
 reflen(alns::AlignedReads, pindexpair::Tuple{Int,Int}) =
     max(alns.leftpos[first(pindexpair)], alns.leftpos[last(pindexpair)], alns.rightpos[first(pindexpair)], alns.rightpos[last(pindexpair)]) -
     min(alns.leftpos[first(pindexpair)], alns.leftpos[last(pindexpair)], alns.rightpos[first(pindexpair)], alns.rightpos[last(pindexpair)])
--- ChimericAnalysis/src/pipeline.jl
+++ ChimericAnalysis/src/pipeline.jl
@@ -1,6 +1,9 @@
 function mergetypes(features::Features, cds_type::String, five_type::String, three_type::String, mergetype::String)
+    # Compute merged annotations for anntoations of types cds_type, five_type and three_type with the same name
     types = (cds_type, five_type, three_type)
     merged_features = Interval{Annotation}[]
+
+    # Collect all features with the types cds_type, five_type and three_type grouped by the annotation name
     feature_collector = Dict{String,Vector{Interval{Annotation}}}(name(feature)=>Interval{Annotation}[] for feature in features if type(feature) in types)
     for feature in features
         if type(feature) in types
@@ -9,15 +12,23 @@ function mergetypes(features::Features, cds_type::String, five_type::String, thr
             push!(merged_features, feature)
         end
     end
+
+    # Loop through grouped annotations and combine, if name, strand and reference match
     for (n, fs) in feature_collector
         s = strand(fs[1])
         ref = refname(fs[1])
         any((refname(f)!=ref) || (strand(f)!=s) for f in fs) &&
             throw(AssertionError("Features with the same name of types $types have to be on the same reference sequence and strand."))
+
+        # Compute the new borders of the merged annotation
         left = minimum(leftposition(f) for f in fs)
         right = maximum(rightposition(f) for f in fs)
+
+        # Can only merge if a cds_type is in the grouped annotations
         any(type(f) == cds_type for f in fs) || continue
         cds = first((s == STRAND_POS ? leftposition : rightposition)(f) for f in fs if type(f) == cds_type)
+
+        # Save the position of the first nucleotide of the CDS for later use in visualisation
         ann = Dict("Name"=>n, "cds"=>isnothing(cds) ? "NA" : "$cds")
         push!(merged_features, Interval(ref, left, right, s, Annotation(mergetype, n, ann)))
     end
@@ -25,6 +36,7 @@ function mergetypes(features::Features, cds_type::String, five_type::String, thr
 end
 
 function Base.write(fname::String, files::SingleTypeFiles)
+    # Convert a set of CSV files into an XLSX file with all CSVs as sheets within.
     files.type in (".csv",) || throw(AssertionError("File type has to be .csv"))
     if files.type == ".csv"
         tables = Vector{Tuple{String,Vector{Any},Vector{String}}}()
@@ -63,29 +75,36 @@ function chimeric_analysis(features::Features, bams::SingleTypeFiles, results_pa
                             filter_name_queries=[],
                             join_pvalues_method=:stouffer)
 
+    # Set up logger. It saves all @info and @warn messages and also outputs them to the terminal.
     filelogger = FormatLogger(joinpath(results_path, "analysis.log"); append=true) do io, args
         println(io, "[", args.level, "] ", args.message)
     end
     with_logger(TeeLogger(filelogger, ConsoleLogger())) do
         @info "Starting new analysis..."
 
+        # Set up the AffineGapScoreModel used to compute the basepairing predictions. Parameters are defined in the config.jl
         scores = Dict((DNA_A, DNA_T)=>bp_parameters[1], (DNA_T, DNA_A)=>bp_parameters[1],
                     (DNA_C, DNA_G)=>bp_parameters[2], (DNA_G, DNA_C)=>bp_parameters[2],
                     (DNA_G, DNA_T)=>bp_parameters[3], (DNA_T, DNA_G)=>bp_parameters[3])
         model = AffineGapScoreModel(SubstitutionMatrix(scores; default_match=-1*bp_parameters[4], default_mismatch=-1*bp_parameters[4]);
             gap_open=-1*bp_parameters[5], gap_extend=-1*bp_parameters[6])
 
+        # The length of the sequences used for computing complementarity in the random model and the experiment. Parameters are defined in the config.jl
         seq_length = check_interaction_distances[1]-check_interaction_distances[2]+1
 
+        # Compute a empirical density of the complementarity scores for randomly sampled pairs of sequences of fixed length.
         genome_model_ecdf = ecdf([score_bp(pairalign(LocalAlignment(),
             i1 % 2 == 0 ? genome.seq[i1:i1+seq_length-1] : reverse_complement(genome.seq[i1:i1+seq_length-1]),
             i2 % 2 == 0 ? reverse(genome.seq[i2:i2+seq_length-1]) : complement(genome.seq[i2:i2+seq_length-1]),
             model), shift_weight) for (i1, i2) in eachrow(rand(1:(length(genome.seq)-seq_length), (n_genome_samples,2)))]
         )
 
+        # Set up folder structure for the results
         @info "Using $(summarize(features))"
         isdir(joinpath(results_path, "tables")) || mkpath(joinpath(results_path, "tables"))
         isdir(joinpath(results_path, "jld")) || mkpath(joinpath(results_path, "jld"))
+
+        # Cycle through all conditions. The are defined in the config.jl as samplename_condition
         for (condition, r) in conditions
 
             replicate_ids = Vector{Symbol}()
@@ -93,27 +112,35 @@ function chimeric_analysis(features::Features, bams::SingleTypeFiles, results_pa
             GC.gc()
 
             @info "Collecting $(length(r)) samples for condition $condition:"
+
+            # Reuse existing analysis results if present
             if isfile(joinpath(results_path, "jld", "$(condition).jld2"))
                 @info "Found results files. Using existing JLD2 file for $condition..."
                 interactions = Interactions(joinpath(results_path, "jld", "$(condition).jld2"))
             else
+                # Cycle through all replicates in the current condition. The are defined in the config.jl as samplename_condition
                 for (i, bam) in enumerate(bams[r])
                     replicate_id = Symbol("$(condition)_$i")
                     push!(replicate_ids, replicate_id)
                     @info "Replicate $replicate_id:"
+
                     @info "Reading $bam"
+                    # Use the AlignedReads constructor from RNASeqTools to read alignments and sort them according to their position on the read.
                     alignments = AlignedReads(bam; include_secondary_alignments=include_secondary_alignments,
                                             include_alternative_alignments=include_alternative_alignments,
                                             is_reverse_complement=is_reverse_complement)
 
                     @info "Annotating alignments..."
+                    # Use annotate! from RNASeqTools to efficiently and uniquely match the annotation with largest overlap to each alignment.
                     annotate!(alignments, features; prioritize_type=prioritize_type, min_prioritize_overlap=min_prioritize_overlap,
                                                     overwrite_type=overwrite_type)
 
                     @info "Building graph of interactions..."
+                    # Call append! from interactions.jl to collect all interactions with their ligation points from the alignments.
                     append!(interactions, alignments, replicate_id; min_distance=min_distance, max_ligation_distance=max_ligation_distance,
                         filter_types=filter_types, allow_self_chimeras=allow_self_chimeras, is_paired_end=is_paired_end)
 
+                    # Memory management
                     empty!(alignments)
                     GC.gc()
                 end
@@ -124,36 +151,46 @@ function chimeric_analysis(features::Features, bams::SingleTypeFiles, results_pa
             @info "Found $(length(interactions.multichimeras)) unique multichimeric arrangements."
 
             @info "Running statistical tests..."
+            # Add infromation on relative positions of interactions within their annotations. Defined in interactions.jl
             addpositions!(interactions, features)
+
+            # Do statistical tests. Defined in interactions.jl
             addpvalues!(interactions, genome, genome_model_ecdf; include_singles=include_singles, include_read_identity=include_read_identity,
                     fisher_exact_tail=fisher_exact_tail, check_interaction_distances=check_interaction_distances, bp_parameters=bp_parameters,
                     shift_weight=shift_weight, join_pvalues_method=join_pvalues_method)
 
+            # Compute stats on significant interactions and read counts and filter data
             total_reads = sum(interactions.edges[!, :nb_ints])
             total_ints = nrow(interactions.edges)
 
+            # Filter data according to query strings which are looked up in the annotation names
             if !isempty(filter_name_queries)
                 filterset = Set(findall(foldl(.|, occursin.(q, interactions.nodes.name) for q in filter_name_queries)))
                 filter!([:src, :dst] => (x, y) -> !((x in filterset) || (y in filterset)), interactions.edges)
             end
 
+            # Filter according to read count cutoff
             above_min_reads = sum(interactions.edges[interactions.edges.nb_ints .>= min_reads, :nb_ints])
             above_min_ints = sum(interactions.edges.nb_ints .>= min_reads)
             filter!(:nb_ints => x -> x >= min_reads, interactions.edges)
 
+            # Filter according to fishers exact test FDR cutoff
             total_sig_reads = sum(interactions.edges[interactions.edges.fisher_fdr .<= max_fisher_fdr, :nb_ints])
             total_sig_ints = sum(interactions.edges.fisher_fdr .<= max_fisher_fdr)
             filter!(:fisher_fdr => x -> x <= max_fisher_fdr, interactions.edges)
 
+            # Filter according to complementarity FDR cutoff
             above_min_bp_reads = sum(interactions.edges[interactions.edges.bp_fdr .<= max_bp_fdr, :nb_ints])
             above_min_bp_ints = sum(interactions.edges.bp_fdr .<= max_bp_fdr)
             filter!(:bp_fdr => x -> (x <= max_bp_fdr) | (isnan(x) & keep_ints_without_ligation), interactions.edges)
 
+            # Print pretty table
             infotable = DataFrame(""=>["total interactions:", "annotation pairs:"], "total"=>[total_reads, total_ints], "reads>=$min_reads"=>[above_min_reads, above_min_ints],
                 "& fdr<=$max_fisher_fdr"=>[total_sig_reads, total_sig_ints], "& bp_fdr<=$max_bp_fdr"=>[above_min_bp_reads, above_min_bp_ints])
 
             @info "interaction stats for condition $condition:\n" * DataFrames.pretty_table(String, infotable, nosubheader=true)
 
+            # Check if interactions are left after filtering, compute correlation between interaction counts in replicates
             if nrow(interactions.edges) > 0
                 correlation_matrix = corspearman(Matrix(interactions.edges[:, interactions.replicate_ids]))
                 correlation_df = DataFrame(replicate_ids=interactions.replicate_ids)
@@ -167,15 +204,19 @@ function chimeric_analysis(features::Features, bams::SingleTypeFiles, results_pa
 
             @info "Saving..."
 
+            # Save interaction data in table form
             odf = asdataframe(interactions; output=:edges, min_reads=min_reads, max_fisher_fdr=max_fisher_fdr, max_bp_fdr=max_bp_fdr)
             CSV.write(joinpath(results_path, "tables", "interactions_$(condition).csv"), odf)
 
+            # Save summary data per annotation in table form
             odf = asdataframe(interactions; output=:nodes, min_reads=min_reads, max_fisher_fdr=max_fisher_fdr, max_bp_fdr=max_bp_fdr)
             CSV.write(joinpath(results_path, "tables", "genes_$(condition).csv"), odf)
 
+            # Save all found multi-chimeras into a table
             odf = asdataframe(interactions; output=:multi)
             CSV.write(joinpath(results_path, "tables", "multi_$(condition).csv"), odf)
 
+            # Save the Interactions struct to JLD2 file
             write(joinpath(results_path, "jld", "$(condition).jld2"), interactions)
 
         end
